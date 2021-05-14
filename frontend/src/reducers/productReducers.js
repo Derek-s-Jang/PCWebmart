2b31d61f4f606fe469020a4ba085735d3049ae54
Derek-s-Jang <drkjng54@gmail.com>
2021-05-14 14:32:48 -0400

    Add Redux to Product Screen

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,10 +1,10 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
 
 export const productListReducer = (
-    state = { loading: true, products: [] }, 
+    state = { loading: true, products: [] },
     action
 ) => {
-    switch(action.type){
+    switch (action.type) {
         case PRODUCT_LIST_REQUEST:
             return { loading: true };
         case PRODUCT_LIST_SUCCESS:
@@ -14,4 +14,20 @@ export const productListReducer = (
         default:
             return state;
     }
-}
+};
+
+export const productDetailsReducer = (
+    state = { loading: true, products: [] },
+    action
+) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return { loading: true };
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload };
+        case PRODUCT_DETAILS_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,7 +7,7 @@ import { listProducts } from '../actions/productActions';
 
 export default function HomeScreen() {
     const dispatch = useDispatch();
-    const productList = useSelector( state => state.productList);
+    const productList = useSelector(state => state.productList);
     const { loading, error, products } = productList;
 
     useEffect(() => {
@@ -16,7 +16,7 @@ export default function HomeScreen() {
     return (
         <div>
             {loading ? (
-                <LoadingBox></LoadingBox> 
+                <LoadingBox></LoadingBox>
             ) : error ? (
                 <MessageBox variant="danger">{error}</MessageBox>
             ) : (
